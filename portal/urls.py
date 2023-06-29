d97400bbdf9544b30a4c642b7e33feafb7ef1d0f
LHA8HC <hai.lehuynhlong@vn.bosch.com>
2023-06-29 14:02:57 +0700

    add thankyou page

--- portal/urls.py
+++ portal/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.portal, name='portal')
+    path('', views.portal, name='portal'),
+    path('thankyou', views.thankyou, name='thankyou')
 ]
--- portal/views.py
+++ portal/views.py
@@ -5,3 +5,8 @@ from django.template import loader
 def portal(request):
     template = loader.get_template('portal.html')
     return HttpResponse(template.render())
+
+
+def thankyou(request):
+    template = loader.get_template('thankyou.html')
+    return HttpResponse(template.render())
